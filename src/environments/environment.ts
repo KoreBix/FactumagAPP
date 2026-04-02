85d98e17a06fd9b24b4c2146cb8cbed93de21bef
Manuel Arriaga <manuelmag-@hotmail.com>
2026-04-02 10:19:30 -0600

    Logo

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,15 @@
+
 export const environment = {
   production: false,
   authUrl:         'https://localhost:7054/',//'http://localhost:5001',
   facturacionUrl:  'https://localhost:7282',//'http://localhost:5003'
   ssoUrl:     'http://localhost:4200'
 };
+
+/*
+export const environment = {
+  production: true,
+  authUrl:         'https://apisuites.betakorebix.com/',
+  facturacionUrl:  'https://apifacturacion.betakorebix.com',
+  ssoUrl:     'https://betakorebix.com'
+};*/
